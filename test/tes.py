b02ba0a01dc7ff05587f80f8a38094b5236e9c76
pdream <gaoxuan@gaox-lab.com>
2020-02-02 18:45:26 +0800

    Initial commit

---
+++ test/tes.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3.7
+# _*_ coding=utf-8 _*_
+# 测试环境
+# 由系统生成
+
---
+++ test/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.7
+# _*_ coding=utf-8 _*_
+# 测试环境
+# 由系统生成
+
+print ("hello world")
+
+test tests testsw cisco3DES
+test tests testsw cisco3DES
